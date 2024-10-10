acd867874bc7c164fd68dbe4402d0b61813cf0f1
Gabriel Tsei <92765565+BielTsei@users.noreply.github.com>
2024-10-10 18:50:23 -0300

    Arrumado menu e comprenssado JS e CSS

--- js/mobile.js
+++ js/mobile.js
@@ -1,13 +1,1 @@
-const btnMobile = document.getElementById("btn-mobile");
-function toogleMenu(t) {
-  "touchstart" === t.type && t.preventDefault();
-  const e = document.getElementById("nav");
-  e.classList.toggle("active");
-  var n = e.classList.contains("active");
-  t.currentTarget.setAttribute("aria-expanded", n),
-    n
-      ? t.currentTarget.setAttribute("aria-label", "Fechar menu")
-      : t.currentTarget.setAttribute("aria-label", "Abrir menu");
-}
-btnMobile.addEventListener("click", toogleMenu),
-  btnMobile.addEventListener("touchstart", toogleMenu);
+const btnMobile=document.getElementById("btn-mobile");function toggleMenu(e){"touchstart"===e.type&&e.preventDefault();const t=document.getElementById("nav");t.classList.toggle("active");var n=t.classList.contains("active");e.currentTarget.setAttribute("aria-expanded",n),n?e.currentTarget.setAttribute("aria-label","Fechar menu"):e.currentTarget.setAttribute("aria-label","Abrir menu")}function closeMenu(){const e=document.getElementById("nav");e.classList.remove("active"),btnMobile.setAttribute("aria-expanded","false"),btnMobile.setAttribute("aria-label","Abrir menu")}btnMobile.addEventListener("click",toggleMenu),btnMobile.addEventListener("touchstart",toggleMenu);const menuLinks=document.querySelectorAll("#menu a");menuLinks.forEach(e=>{e.addEventListener("click",closeMenu)});
